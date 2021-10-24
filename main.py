ececdf9a997459ac693d10231fa50661bc7b851b
tudorsuiu <tudorsuiu21@gmail.com>
2021-10-24 15:16:32 +0300

    Domain, Logic, Tests, UI

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from Logic.CRUD import add_obiect
 from Tests.test_all import run_all_tests
 from UI.console import run_menu
 
 
 def main():
     run_all_tests()
-    run_menu([])
+    inventar = []
+    inventar = add_obiect("1", "Laptop", "Tehnologie", 1400.0, "E1C2", inventar)
+    inventar = add_obiect("2", "Telefon", "Tehnologie", 1000.0, "E1C1", inventar)
+    run_menu(inventar)
 
 
 if __name__ == "__main__":
