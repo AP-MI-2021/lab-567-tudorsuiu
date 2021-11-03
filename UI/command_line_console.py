7ef822151a5de63e76cf728c27c18e5361f81cf6
tudorsuiu <tudorsuiu21@gmail.com>
2021-11-03 08:05:03 +0200

    Lab 6

--- UI/command_line_console.py
+++ UI/command_line_console.py
@@ -1,2 +1,31 @@
-def print_menu():
-    print("")
+from typing import List
+
+from Domain.object import to_string
+from Logic.CRUD import add_obiect, delete_obiect
+
+
+def print_description():
+    print("Lista de comenzi va fi separata prin ','")
+    print("Comenzile pot fi: add, showall, delete.")
+    print("Parametrii fiecarei comenzi vor fi separati prin ','")
+
+
+def show_all(inventar: List[dict]):
+    for obiect in inventar:
+        print(to_string(obiect))
+
+
+def run_command_line_console(inventar: List[dict]):
+    print_description()
+    list_input = input("Dati lista de comenzi: ")
+    list_with_commands = list_input.split(",")
+    while True:
+        for i in range(len(list_with_commands)):
+            if list_with_commands[i] == "add":
+                inventar = add_obiect(list_with_commands[i+1], list_with_commands[i+2], list_with_commands[i+3],
+                                      float(list_with_commands[i+4]), list_with_commands[i+5], inventar)
+            elif list_with_commands[i] == "delete":
+                inventar = delete_obiect(list_with_commands[i+1], inventar)
+            elif list_with_commands[i] == "showall":
+                show_all(inventar)
+        break
--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from Logic.CRUD import add_obiect
 from Tests.test_all import run_all_tests
+from UI.command_line_console import run_command_line_console
 from UI.console import run_menu
 
 
 def main():
     run_all_tests()
     inventar = []
-    run_menu(inventar)
+    type_of_menu = input("Ce de tip de consola doriti sa se foloseasca? (C/CLC) ")
+    if type_of_menu == "C":
+        run_menu(inventar)
+    elif type_of_menu == "CLC":
+        run_command_line_console(inventar)
+    else:
+        print("Acest tip de consola nu exista! Alegeti dintre C - Console sau CLC - Command Line Console")
 
 
 if __name__ == "__main__":
