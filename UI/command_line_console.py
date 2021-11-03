da9b164125fbcdf509234e688c3a78701fb68083
tudorsuiu <tudorsuiu21@gmail.com>
2021-11-03 08:29:37 +0200

    Lab 6 CLC

--- UI/command_line_console.py
+++ UI/command_line_console.py
@@ -6,7 +6,7 @@ from Logic.CRUD import add_obiect, delete_obiect
 
 def print_description():
     print("Lista de comenzi va fi separata prin ','")
-    print("Comenzile pot fi: add, showall, delete.")
+    print("Comenzile pot fi: add, showall, delete, exit.")
     print("Parametrii fiecarei comenzi vor fi separati prin ','")
 
 
@@ -17,15 +17,18 @@ def show_all(inventar: List[dict]):
 
 def run_command_line_console(inventar: List[dict]):
     print_description()
-    list_input = input("Dati lista de comenzi: ")
-    list_with_commands = list_input.split(",")
-    while True:
+    should_run = True
+    while should_run:
+        list_input = input("Dati lista de comenzi: ")
+        list_with_commands = list_input.split(",")
         for i in range(len(list_with_commands)):
-            if list_with_commands[i] == "add":
-                inventar = add_obiect(list_with_commands[i+1], list_with_commands[i+2], list_with_commands[i+3],
-                                      float(list_with_commands[i+4]), list_with_commands[i+5], inventar)
+            if list_with_commands[i] == "exit":
+                should_run = False
+                break
+            elif list_with_commands[i] == "add":
+                inventar = add_obiect(list_with_commands[i + 1], list_with_commands[i + 2], list_with_commands[i + 3],
+                                      float(list_with_commands[i + 4]), list_with_commands[i + 5], inventar)
             elif list_with_commands[i] == "delete":
-                inventar = delete_obiect(list_with_commands[i+1], inventar)
+                inventar = delete_obiect(list_with_commands[i + 1], inventar)
             elif list_with_commands[i] == "showall":
                 show_all(inventar)
-        break
