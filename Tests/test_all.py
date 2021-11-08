941d988bab45acbce6995324b90528e98c62fb06
tudorsuiu <tudorsuiu21@gmail.com>
2021-11-08 22:15:03 +0200

    Lab 7

--- Tests/test_all.py
+++ Tests/test_all.py
@@ -3,7 +3,8 @@ from Tests.test_domain import test_obiect
 from Tests.test_functionalities import test_move_all_obiecte_to_another_locatie, \
     test_concatenation_to_all_obiecte_above_price, test_determine_maximum_price_for_every_locatie, \
     test_ascending_sorting_by_price, test_sum_for_every_location
-from Tests.test_undo_redo import test_undo_redo
+from Tests.test_undo_redo import test_undo_redo, test_undo_redo_move_all_obiecte_to_another_locatie, \
+    test_undo_redo_concatenation_to_all_obiecte_above_price, test_undo_redo_ascending_sorting_by_price
 
 
 def run_all_tests():
@@ -18,3 +19,6 @@ def run_all_tests():
     test_ascending_sorting_by_price()
     test_sum_for_every_location()
     test_undo_redo()
+    test_undo_redo_move_all_obiecte_to_another_locatie()
+    test_undo_redo_concatenation_to_all_obiecte_above_price()
+    test_undo_redo_ascending_sorting_by_price()
--- Tests/test_undo_redo.py
+++ Tests/test_undo_redo.py
@@ -1,5 +1,67 @@
 from Logic.CRUD import add_obiect
-from Logic.functionalities import undo, redo
+from Logic.functionalities import undo, redo, move_all_obiecte_to_another_locatie, \
+    concatenation_to_all_obiecte_above_price, ascending_sorting_by_price
+
+
+def test_undo_redo_move_all_obiecte_to_another_locatie():
+    inventar = []
+    undo_list = []
+    redo_list = []
+    inventar = add_obiect("1", "o1", "o1", 1, "o1", inventar)
+    undo_list.append(inventar)
+    redo_list.clear()
+    inventar = move_all_obiecte_to_another_locatie(inventar, "P")
+    assert inventar == [{"id": "1", "nume": "o1", "descriere": "o1", "pret_achizitie": 1, "locatie": "P"}]
+    inventar = undo(inventar, undo_list, redo_list)
+    assert inventar == [{"id": "1", "nume": "o1", "descriere": "o1", "pret_achizitie": 1, "locatie": "o1"}]
+    inventar = redo(inventar, undo_list, redo_list)
+    assert inventar == [{"id": "1", "nume": "o1", "descriere": "o1", "pret_achizitie": 1, "locatie": "P"}]
+
+
+def test_undo_redo_concatenation_to_all_obiecte_above_price():
+    inventar = []
+    undo_list = []
+    redo_list = []
+    inventar = add_obiect("1", "o1", "o1", 1, "o1", inventar)
+    inventar = add_obiect("2", "o2", "o2", 2, "o2", inventar)
+    inventar = add_obiect("3", "o3", "o3", 3, "o3", inventar)
+    undo_list.append(inventar)
+    redo_list.clear()
+    inventar = concatenation_to_all_obiecte_above_price(inventar, "t", 1)
+    assert inventar == [{"id": "1", "nume": "o1", "descriere": "o1", "pret_achizitie": 1, "locatie": "o1"},
+                        {"id": "2", "nume": "o2", "descriere": "o2t", "pret_achizitie": 2, "locatie": "o2"},
+                        {"id": "3", "nume": "o3", "descriere": "o3t", "pret_achizitie": 3, "locatie": "o3"}]
+    inventar = undo(inventar, undo_list, redo_list)
+    assert inventar == [{"id": "1", "nume": "o1", "descriere": "o1", "pret_achizitie": 1, "locatie": "o1"},
+                        {"id": "2", "nume": "o2", "descriere": "o2", "pret_achizitie": 2, "locatie": "o2"},
+                        {"id": "3", "nume": "o3", "descriere": "o3", "pret_achizitie": 3, "locatie": "o3"}]
+    inventar = redo(inventar, undo_list, redo_list)
+    assert inventar == [{"id": "1", "nume": "o1", "descriere": "o1", "pret_achizitie": 1, "locatie": "o1"},
+                        {"id": "2", "nume": "o2", "descriere": "o2t", "pret_achizitie": 2, "locatie": "o2"},
+                        {"id": "3", "nume": "o3", "descriere": "o3t", "pret_achizitie": 3, "locatie": "o3"}]
+
+
+def test_undo_redo_ascending_sorting_by_price():
+    inventar = []
+    undo_list = []
+    redo_list = []
+    inventar = add_obiect("1", "o1", "o1", 3, "o1", inventar)
+    inventar = add_obiect("2", "o2", "o2", 2, "o2", inventar)
+    inventar = add_obiect("3", "o3", "o3", 1, "o3", inventar)
+    undo_list.append(inventar)
+    redo_list.clear()
+    inventar = ascending_sorting_by_price(inventar)
+    assert inventar == [{"id": "3", "nume": "o3", "descriere": "o3", "pret_achizitie": 1, "locatie": "o3"},
+                        {"id": "2", "nume": "o2", "descriere": "o2", "pret_achizitie": 2, "locatie": "o2"},
+                        {"id": "1", "nume": "o1", "descriere": "o1", "pret_achizitie": 3, "locatie": "o1"}]
+    inventar = undo(inventar, undo_list, redo_list)
+    assert inventar == [{"id": "1", "nume": "o1", "descriere": "o1", "pret_achizitie": 3, "locatie": "o1"},
+                        {"id": "2", "nume": "o2", "descriere": "o2", "pret_achizitie": 2, "locatie": "o2"},
+                        {"id": "3", "nume": "o3", "descriere": "o3", "pret_achizitie": 1, "locatie": "o3"}]
+    inventar = redo(inventar, undo_list, redo_list)
+    assert inventar == [{"id": "3", "nume": "o3", "descriere": "o3", "pret_achizitie": 1, "locatie": "o3"},
+                       {"id": "2", "nume": "o2", "descriere": "o2", "pret_achizitie": 2, "locatie": "o2"},
+                       {"id": "1", "nume": "o1", "descriere": "o1", "pret_achizitie": 3, "locatie": "o1"}]
 
 
 def test_undo_redo():
